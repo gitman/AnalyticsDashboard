41c69d7bde51460b75fd5f5c81366d302dbee77e
Girish Tryambake <girish.tryambake@exceed.co.in>
2014-02-07 12:27:11 +0530

    [1m[36m removed from log

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module AnalyticsDashboard
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.colorize_logging = false
   end
 end
